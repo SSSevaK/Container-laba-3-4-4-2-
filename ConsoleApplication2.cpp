52926594d4fe267f01ae0f76e89c21b6cc222db7
LLIARF1K 雨 <wispvsolo@gmail.com>
2025-11-09 20:13:13 +0300

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,386 @@
+﻿#include <iostream>
+#include <ctime>
+#include <random>
+#include <cmath>
+#include <array>
+#include <vector>
+#include <algorithm>
+
+template<class T>
+bool is_correct(T&);
+template<class T>
+bool check_size(T);
+template<typename Container>
+bool input_mass(int&, double&, int, Container&);
+template<typename Container>
+double min_element(int, Container&);
+template<typename Container>
+void min_numb_elem(double, int, Container&);
+template<typename Container>
+bool negative_number(int, Container&, int&, int&);
+template<typename Container>
+void SumBetNegNumb(int, Container&);
+template<typename Container>
+double sum(int, int, int, Container&);
+template<typename Container>
+void sort_arr(int, double, Container&);
+template<typename Container>
+void print_arr(int, Container&);
+template<typename Container>
+void rand_numb(int, int, int, Container&);
+template<typename Container>
+bool randNumb_function(short, Container&);
+template<typename Container>
+bool inputNumb_function(short, Container&);
+int input_sizeOfArr(short&);
+int input_typeOfArr(short&);
+bool rundfunc(short, short);
+bool staticfunc(short, short);
+
+int main()
+{
+    short type{ 0 };
+    short size{ 0 };
+    short type_mass{ 0 };
+    try
+    {
+        std::cout << "if you wont static mass input 1, if you wont dinamic imput 2" << '\n';
+        if (!input_sizeOfArr(type_mass))
+        {
+            throw false;
+        }
+        if (type_mass == 2)
+        {
+            rundfunc(size, type);
+        }
+        if (type_mass == 1)
+        {
+            staticfunc(size, type);
+        }
+        if (type_mass != 1 && type_mass != 2)
+        {
+            return false;
+        }
+    }
+    catch (bool)
+    {
+        std::cout << "mistake";
+    }
+    return 0;
+}
+
+template<class T>
+bool is_correct(T& number)
+{
+    return (std::cin >> number) ? true : false;
+}
+template<class T>
+bool check_size(T number)
+{
+    return (number > 0 && number < 100) ? true : false;
+}
+template<typename Container>
+bool input_mass(int& prov, double& temp, int size, Container& arr)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (is_correct(temp))
+        {
+            arr[i] = temp;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return true;
+}
+template<typename Container>
+double min_element(int size, Container& arr)
+{
+    double min_number = arr[0];
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] < min_number)
+        {
+            min_number = arr[i];
+        }
+    }
+    return min_number;
+}
+template<typename Container>
+void min_numb_elem(double min, int size, Container& arr)
+{
+    for (int i = size - 1; i >= 0; i--)
+    {
+        if (arr[i] == min)
+        {
+            std::cout << i + 1 << '\n';
+        }
+    }
+}
+template<typename Container>
+bool negative_number(int size, Container& arr, int& min, int& max)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] < 0)
+        {
+            min = i;
+            for (int j = size - 1; j >= 0; j--)
+            {
+                if (arr[j] < 0)
+                {
+                    max = j;
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+template<typename Container>
+double sum(int min, int max, int size, Container& arr)
+{
+    double sum_numb{ 0 };
+    for (int i = min + 1; i < max; i++)
+    {
+        sum_numb += arr[i];
+    }
+    return sum_numb;
+}
+template<typename Container>
+void sort_arr(int size, double X,Container& arr)
+{
+    std::sort(arr.begin(), arr.end());
+}
+template<typename Container>
+void print_arr(int size, Container& arr)
+{
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << arr[i] << '\n';
+    }
+}
+template<typename Container>
+void rand_numb(int a, int b, int size, Container& arr)
+{
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand() % (b - a + 1) + a;
+    }
+}
+template<typename Container>
+bool randNumb_function(short size, Container& arr)
+{
+    short a;
+    short b;
+    try
+    {
+        std::cout << "input range of numbers" << '\n';
+        if (!is_correct(a))
+        {
+            throw false;
+        }
+        if (!is_correct(b))
+        {
+            throw false;
+        }
+        if (a > b)
+        {
+            std::swap(a, b);
+        }
+        srand(time(NULL));
+        rand_numb(a, b, size, arr);
+        std::cout << "array is :" << '\n';
+        print_arr(size, arr);
+        std::cout << "the numbers of the minimum array elements is :" << '\n';
+        min_numb_elem(min_element(size, arr), size, arr);
+        SumBetNegNumb(size, arr);
+        double X;
+        if (!is_correct(X))
+        {
+            throw false;
+        }
+        std::cout << " sorted array " << '\n';
+        sort_arr(size, X, arr);
+        print_arr(size, arr);
+    }
+    catch (bool)
+    {
+        return false;
+    }
+    return true;
+}
+template<typename Container>
+bool inputNumb_function(short size, Container& arr)
+{
+    try
+    {
+        double temp{ 0 };
+        int prov = 1;
+        std::cout << "input " << size << " numbers" << '\n';
+        if (!input_mass(prov, temp, size, arr))
+        {
+            throw false;
+        }
+        std::cout << "the numbers of the minimum array elements is :" << '\n';
+        min_numb_elem(min_element(size, arr), size, arr);
+        SumBetNegNumb(size, arr);
+        double X;
+        std::cout << " input number X. This number need for array sorting " << '\n';
+        if (!is_correct(X))
+        {
+            throw false;
+        }
+        std::cout << " sorted array " << '\n';
+        sort_arr(size, X, arr);
+        print_arr(size, arr);
+    }
+    catch (bool)
+    {
+        return false;
+    }
+    return true;
+}
+int input_sizeOfArr(short& size)
+{
+    try
+    {
+        if (is_correct(size) == 0)
+        {
+            throw false;
+        }
+        if (check_size(size) == 0)
+        {
+            throw false;
+        }
+    }
+    catch (bool)
+    {
+        return 0;
+    }
+    return size;
+}
+int input_typeOfArr(short& type)
+{
+    try
+    {
+        std::cout << "if you want random number press 1 , if you want to enter numbers, enter 2" << '\n';
+        if (is_correct(type) == 0)
+        {
+            throw false;
+        }
+        if (check_size(type) == 0)
+        {
+            throw false;
+        }
+    }
+    catch (bool)
+    {
+        return 0;
+    }
+    return type;
+}
+template<typename Container>
+void SumBetNegNumb(int size, Container& arr)
+{
+    int min = 0;
+    int max = 0;
+    if (!negative_number(size, arr, min, max))
+    {
+        std::cout << "We don't have negative numbers.";
+    }
+    else
+    {
+        if (min == max)
+        {
+            std::cout << "1 neg element, therefore sum = 0 ." << '\n';
+        }
+        else
+        {
+            std::cout << "sum between " << min + 1 << " and " << max + 1 << " elements is " << '\n';
+            std::cout << sum(min, max, size, arr) << '\n';
+        }
+    }
+    std::cout << " input number X. This number need for array sorting " << '\n';
+}
+bool rundfunc(short size, short type)
+{
+    try
+    {
+        std::cout << "input size of mass > 0 but < 100" << '\n';
+        if (!input_sizeOfArr(size))
+        {
+            throw false;
+        }
+        if (!input_typeOfArr(type))
+        {
+            throw false;
+        }
+        std::vector<double> arr(size);
+        if (type == 2)
+        {
+            if (!inputNumb_function(size, arr))
+            {
+                throw false;
+            }
+        }
+        if (type == 1)
+        {
+            if (!randNumb_function(size, arr))
+            {
+                throw false;
+            }
+        }
+        if (type != 1 && type != 2)
+        {
+            throw false;
+        }
+    }
+    catch (bool)
+    {
+        return false;
+    }
+    return true;
+}
+bool staticfunc(short size, short type)
+{
+    const short Max_size{ 100 };
+    std::array<double, Max_size> arr{};
+    try
+    {
+        std::cout << "input size of mass > 0 but < 100" << '\n';
+        if (!input_sizeOfArr(size))
+        {
+            throw false;
+        }
+        if (!input_typeOfArr(type))
+        {
+            throw false;
+        }
+        if (type == 2)
+        {
+            if (!inputNumb_function(size, arr))
+            {
+                throw false;
+            }
+        }
+        if (type == 1)
+        {
+            if (!randNumb_function(size, arr))
+            {
+                throw false;
+            }
+        }
+        if (type != 1 && type != 2)
+        {
+            throw false;
+        }
+    }
+    catch (bool)
+    {
+        return false;
+    }
+    return true;
+}
---
+++ vector_laba4.cpp
@@ -0,0 +1,292 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+bool is_correct(int&);
+bool is_natural(int);
+bool Fill_mas(std::vector<std::vector<int>>&, int, int);
+int Sum_elem(std::vector<std::vector<int>>&, int, int);
+int Max_Elem(std::vector<std::vector<int>>&, int, int);
+int Min_Elem(std::vector<std::vector<int>>&, int, int);
+bool prov_row(int&);
+bool prov_col(int&);
+void transposed_Arr(int, int, std::vector<std::vector<int>>&, std::vector<std::vector<int>>&);
+void print_transposed_Arr(int, int, std::vector<std::vector<int>>&);
+void print_Arr(int, int, std::vector<std::vector<int>>&);
+int sum_main_diagonal(std::vector<std::vector<int>>&, int, int);
+void copy(std::vector<std::vector<int>>&, std::vector<std::vector<int>>&, int, int);
+void sort(std::vector<std::vector<int>>&, int, int);
+void swap_row_or_col(std::vector<std::vector<int>>&, int, int, int, int);
+bool numb_max_row(std::vector<std::vector<int>>&, int, int, int, int);
+bool numb_min_row(std::vector<std::vector<int>>&, int, int, int, int);
+
+int main()
+{
+    try
+    {
+        int row{ 0 };
+        int col{ 0 };
+        if (!prov_row(row))
+        {
+            throw false;
+        }
+        if (!prov_col(col))
+        {
+            throw false;
+        }
+        std::vector<std::vector<int>> mas(row,std::vector<int>(col));
+        if (!Fill_mas(mas, row, col))
+        {
+            throw false;
+        }
+        int max = Max_Elem(mas, row, col);
+        int min = Min_Elem(mas, row, col);
+        std::cout << "sum E elements around the perimeter " << Sum_elem(mas, row, col) << '\n';
+        std::cout << "min element is " << min << '\n';
+        std::cout << "max element is " << max << '\n';
+        std::vector<std::vector<int>> arr(col, std::vector<int>(row));
+        transposed_Arr(row, col, mas, arr);
+        std::cout << "this is transposed array : " << '\n';
+        print_transposed_Arr(row, col, arr);
+        std::vector<std::vector<int>> arr1(row, std::vector<int>(col));
+        copy(mas, arr1, row, col);
+        sort(arr1, row, col);
+        std::cout << "this is sort array : " << '\n';
+        print_Arr(row, col, arr1);
+        std::cout << "sum main diagonal : " << sum_main_diagonal(mas, row, col) << '\n';
+        int row_1 = 0;
+        int row_2 = 0;
+        if (numb_max_row(mas, row, col, max, row_1) != 0 && numb_min_row(mas, row, col, min, row_2) != 0)
+        {
+            swap_row_or_col(mas, row_1, row_2, row, col);
+            std::cout << "this is swap array : " << '\n';
+            print_Arr(row, col, mas);
+        }
+        else
+        {
+            std::cout << "max or min numbers more then 1, Unable to sort array";
+        }
+
+    }
+    catch (bool)
+    {
+        std::cout << "mistake";
+    }
+    return 0;
+}
+
+bool Fill_mas(std::vector<std::vector<int>>& mas, int row, int col)
+{
+    std::cout << "Input elements of the array:\n";
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            std::cout << "Element [" << i << "][" << j << "]: ";
+            if (!is_correct(mas[i][j]))
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+bool is_correct(int& number)
+{
+    return(std::cin >> number) ? true : false;
+}
+bool is_natural(int number)
+{
+    return(number > 0) ? true : false;
+}
+int Sum_elem(std::vector<std::vector<int>>& mas, int row, int col)
+{
+    int sum = 0;
+    for (int j = 0; j < col; j++)
+    {
+        sum += mas[0][j];
+        if (row > 1)
+        {
+            sum += mas[row - 1][j];
+        }
+    }
+    for (int i = 1; i < row - 1; i++)
+    {
+        sum += mas[i][0];
+        if (col > 1)
+        {
+            sum += mas[i][col - 1];
+        }
+    }
+    return sum;
+}
+int Min_Elem(std::vector<std::vector<int>>& mas, int row, int col)
+{
+    int min{ 0 };
+    min = mas[0][0];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (min > mas[i][j])
+            {
+                min = mas[i][j];
+            }
+        }
+    }
+    return min;
+}
+int Max_Elem(std::vector<std::vector<int>>& mas, int row, int col)
+{
+    int max{ 0 };
+    max = mas[0][0];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (max < mas[i][j])
+            {
+                max = mas[i][j];
+            }
+        }
+    }
+    return max;
+}
+bool prov_row(int& row)
+{
+    std::cout << "Input the size of a two-dimensional array\n";
+    std::cout << "Number of rows: ";
+    if (is_correct(row) && is_natural(row))
+    {
+        return 1;
+    }
+    return 0;
+}
+bool prov_col(int& col)
+{
+    std::cout << "Number of columns: ";
+    if (is_correct(col) && is_natural(col))
+    {
+        return 1;
+    }
+    return 0;
+}
+void transposed_Arr(int row, int col, std::vector<std::vector<int>>& mas, std::vector<std::vector<int>>& arr)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            arr[j][i] = mas[i][j];
+        }
+    }
+}
+void print_transposed_Arr(int row, int col, std::vector<std::vector<int>>& arr)
+{
+    for (int i = 0; i < col; i++) {
+        for (int j = 0; j < row; j++) {
+            std::cout << arr[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+void print_Arr(int row, int col, std::vector<std::vector<int>>& arr)
+{
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            std::cout << arr[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+int sum_main_diagonal(std::vector<std::vector<int>>& mas, int row, int col)
+{
+    int length = (row < col) ? row : col;
+    int s = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        s += mas[i][i];
+    }
+
+    return s;
+}
+void copy(std::vector<std::vector<int>>& mas, std::vector<std::vector<int>>& arr, int row, int col)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            arr[i][j] = mas[i][j];
+        }
+    }
+}
+void sort(std::vector<std::vector<int>>& arr1, int row, int col)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 1; j < col; j++)
+        {
+            int key = arr1[i][j];
+            int k = j - 1;
+            while (k >= 0 && arr1[i][k] > key)
+            {
+                arr1[i][k + 1] = arr1[i][k];
+                k--;
+            }
+            arr1[i][k + 1] = key;
+        }
+    }
+}
+void swap_row_or_col(std::vector<std::vector<int>>& arr, int max, int min, int row, int col)
+{
+    if (max == min)
+    {
+        for (int i = 0; i < row; i++)
+        {
+            int temp = arr[i][max];
+            arr[i][max] = arr[i][min];
+            arr[i][min] = temp;
+        }
+    }
+    else
+    {
+        for (int j = 0; j < col; j++)
+        {
+            int temp = arr[max][j];
+            arr[max][j] = arr[min][j];
+            arr[min][j] = temp;
+        }
+    }
+}
+bool numb_max_row(std::vector<std::vector<int>>& arr, int row, int col, int max, int row_1)
+{
+    int temp{ 0 };
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (max == arr[i][j])
+            {
+                temp++;
+            }
+        }
+    }
+    return temp == 1 ? true : false;
+}
+bool numb_min_row(std::vector<std::vector<int>>& arr, int row, int col, int min, int row_2)
+{
+    int temp{ 0 };
+    min = arr[0][0];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (min == arr[i][j])
+            {
+                temp++;
+            }
+        }
+    }
+    return temp == 1 ? true : false;
+}
